ee3779c66ddc76c3c38650acfe239841987f20bf
Marc André Audet <root@manhim.net>
2015-03-06 16:35:07 -0500

    The commit that dissapeared

--- library/Cervo/CervoTools.php
+++ library/Cervo/CervoTools.php
@@ -58,7 +58,7 @@ class CervoTools
         // We start the configuration process
 
         _::initConfig($json_config_file);
-        $config = &self::getLibrary('Cervo/Config');
+        $config = &_::getLibrary('Cervo/Config');
 
 
 
