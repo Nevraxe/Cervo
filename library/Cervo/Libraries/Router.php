5ca9d69339b7648f73b59917265236228d7d1298
Marc André Audet <root@manhim.net>
2015-06-09 15:46:59 -0400

    Added a way to override the current Route

--- library/Cervo/Libraries/Router.php
+++ library/Cervo/Libraries/Router.php
@@ -403,4 +403,10 @@ class Router
     {
         return $this->prevent_default;
     }
+
+    public function overrideRoute(Route $route)
+    {
+        $this->route = $route;
+        $this->runEvents();
+    }
 }
